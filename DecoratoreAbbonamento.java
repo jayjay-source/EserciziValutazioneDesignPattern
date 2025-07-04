ade3b750fb840b570654b0dffe0ecb7342123d6a
jayjay-source <jacopofilippi00@gmail.com>
2025-07-04 11:22:43 +0200

    Cancellati file in più

--- DecoratoreAbbonamento.java
+++
@@ -1,7 +0,0 @@
-public abstract class DecoratoreAbbonamento implements Abbonamento{
-    protected Abbonamento abbonamento;
-
-    public DecoratoreAbbonamento(Abbonamento abbonamento) {
-        this.abbonamento = abbonamento;
-    }
-}
